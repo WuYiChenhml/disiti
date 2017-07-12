a793e52e8fba04112ffda5a77c1d1f6f7d0fa345
WuYiChenhml <1055200502@qq.com>
2017-07-12 16:29:07 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,3 +1,13 @@
 let Person = require('./person')
 // Write your code here
+
+var Student = new Object();
+Student.name = "Tom";
+Student.age = 21;
+Student.class = 2;
+
+Student.say = function() {
+	console.log('My name is Tom. I am 21 years old. I am a Student. I am at Class 2.');
+}
+
 module.exports = Student

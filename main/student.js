4078e8a3cc92afebfd3d8b024b60b43ac5572db3
WuYiChenhml <1055200502@qq.com>
2017-07-12 16:43:18 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,13 +1,14 @@
 let Person = require('./person')
 // Write your code here
 
-var Student = new Object();
-Student.name = "Tom";
-Student.age = 21;
-Student.class = 2;
-
-Student.Introduce = function() {
-	console.log('My name is Tom. I am 21 years old. I am a Student. I am at Class 2.');
+function Student() {
+     this.name = "Tom";   
+     this.age = 21;
+	this.class = 2;
+     this.Introduce = function () {
+         console.log('My name is Tom. I am 21 years old. I am a Student. I am at Class 2.');
+     }
+    
 }
 
 module.exports = Student

1c979850b75aac7d6c698aaa3085d8e6acd987e6
WuYiChenhml <1055200502@qq.com>
2017-07-12 17:50:26 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -2,7 +2,8 @@
 function Person(name，age){
    this.name = "Tom";//姓名属性
    this.age = 21;//年龄属性
-   this.Introduce: function (){console.log('My name is Tom. I am 21 years old.')}
+   this.Introduce: function (){
+      console.log('My name is Tom. I am 21 years old.')；}
 }
 var p = new Person();
 Person.Introduce();  

879017b338936db5c05c3fd4d2bdf357c7813a27
WuYiChenhml <1055200502@qq.com>
2017-07-12 14:38:22 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,2 +1,12 @@
 // Write your code here
-module.exports = Person
+module.exports = var Person = 
+ {
+    name: "Tom",
+    age: 21,
+    Introduce: function () 
+  { 
+    alert("My name is " + this.name + ".I'm " + this.age + "years old.");
+  }
+    
+};
+ person.Introduce();  

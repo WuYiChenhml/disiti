6724a4420be25809c16df70becad3c9e23810697
WuYiChenhml <1055200502@qq.com>
2017-07-12 21:19:21 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -2,9 +2,10 @@ let Person = require('./person')
 // Write your code here
 
 function Student(name，age，Class) {
-     this.name = 'Tom';
-     this.age = 21;
-     this.class = 2;
+      this.method = Person; 
+      this.method(name，age);
+      delete this.method; 
+      this.class = 2;
      
      this.introduce = 'My name is ' + this.name + '. I am ' +  this.age + 'years old. I am a Student. I am at Class ' + this.class + ' .';
     return = this.introduce;

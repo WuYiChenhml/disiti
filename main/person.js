4abbee29dbbd25e2e0d778239c382b5af444676e
WuYiChenhml <1055200502@qq.com>
2017-07-12 17:32:00 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,8 +1,9 @@
 // Write your code here
-var Person = {
-    name:"Tom",
-    age:21,
+function Person(){
+   this.name = "Tom";//姓名属性
+   this.age = 21;//年龄属性
    Introduce: function (){console.log(My name is Tom. I am 21 years old.)}
 }; 
+var p = new Person();
 Person.Introduce();  
 module.exports = Person；

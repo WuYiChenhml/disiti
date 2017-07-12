ff5df75a087b7534a9c23ab0f820c850213a476a
WuYiChenhml <1055200502@qq.com>
2017-07-12 20:40:13 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -7,7 +7,7 @@ function = Student(name，age，Class) {
      this.class = 2;
      
      this.introduce = 'My name is ' + this.name + '. I am ' +  this.age + 'years old. I am a Student. I am at Class ' + this.class + ' .';
-    
+    return = this.introduce;
    
 }
     

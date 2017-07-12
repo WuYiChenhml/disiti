abe73de5dd0a448777c1f0195fdcbdc5812e3800
WuYiChenhml <1055200502@qq.com>
2017-07-12 15:58:10 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,10 +1,9 @@
 // Write your code here
-Person = function () {
-	this.name = "Tom";    //通过this关键字设置默认成员
-    this.age = 31;
-    this.Introduce = function () {
-         console.log("My name is " + this.name + ".I'm " + this.age + " years old.");
-   };
-};
-
-module.exports = var Person；
+var Person = {
+                name : "Tom",
+	        age: "21",
+                say_hello : function(){
+                                console.log("My name is Tom. I am 21 years old.")
+                            }
+            }
+module.exports = Person；

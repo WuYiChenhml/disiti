1dc62bd9f6587c922c947481fc8928cccde610ed
WuYiChenhml <1055200502@qq.com>
2017-07-13 12:11:23 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,12 +1,13 @@
 
 // Write your code here
-function Person() {
-     this.name = "dongjc";    
-     this.age = 32;
+function Student() {
+     this.name = "Tom";    
+     this.age = 21;
+     this.class = 2;
      this.Introduce = function () {
-         alert("My name is " + this.name + ".I am " + this.age + "years old.");
+         alert("My name is " + this.name + ".I am " + this.age + "years old. I am a Student. I am at Class " + this.class +'.');
      };
-     alert("My name is " + this.name + ".I'm " + this.age + "years old.");
+     alert("My name is " + this.name + ".I'm " + this.age + "years old. I am a Student. I am at Class " + this.class +'.');
  };
 
 module.exports = Student

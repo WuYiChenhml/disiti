79c52b55b49b0f75fcd151641b1ae8e90453b7d1
WuYiChenhml <1055200502@qq.com>
2017-07-13 12:02:48 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,15 +1,14 @@
 let Person = require('./person')
 // Write your code here
 
-var Student = function Student() {
-      this.name = 'Tom'; 
-      this.age = 21;
-      this.class = 2;
+function Student(name,age,class) {
+      this.name = name; 
+      this.age = age;
+      this.class = class;
      
      this.introduce = function(){
            alert('My name is ' + this.name + '. I am ' +  this.age 
                  + 'years old. I am a Student. I am at Class ' + this.class + ' .');}
-    return this.introduce;
    
 }
 

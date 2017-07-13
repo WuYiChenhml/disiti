21e638955f9b00afd06373152b18b494b4fe28d8
WuYiChenhml <1055200502@qq.com>
2017-07-13 12:12:52 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -3,9 +3,9 @@ function Person() {
      this.name = "Tom";    
      this.age = 21;
      this.Introduce = function () {
-         alert("My name is " + this.name + ".I am " + this.age + "years old.');
+         alert("My name is " + this.name + ".I am " + this.age + "years old.");
      }
-     alert("My name is " + this.name + ".I'm " + this.age + "years old.');
+     alert("My name is " + this.name + ".I'm " + this.age + "years old.");
  };
 
 module.exports = Person

c03a853b47611e3e2aa7442c5ef95e3004cc354a
WuYiChenhml <1055200502@qq.com>
2017-07-13 10:26:52 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -3,7 +3,8 @@ function Person(name，age){
    this.name = name;//姓名属性
    this.age = age;//年龄属性
   
- this. introduce = 'My name is ' + this.name + '. I am ' + this.age + ' years old.';
+ this. introduce = functon(){
+    console.log('My name is ' + this.name + '. I am ' + this.age + ' years old.');}
    return = this.introduce;
 }
 

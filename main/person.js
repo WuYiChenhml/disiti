230bc7bd81c2d25bac8eac2b4f9b3be43bd0caf9
WuYiChenhml <1055200502@qq.com>
2017-07-13 12:12:28 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -4,7 +4,7 @@ function Person() {
      this.age = 21;
      this.Introduce = function () {
          alert("My name is " + this.name + ".I am " + this.age + "years old.');
-     };
+     }
      alert("My name is " + this.name + ".I'm " + this.age + "years old.');
  };
 

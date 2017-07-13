a273c75fc1457d30fb682ccffc5ee4fbd51cd7e6
WuYiChenhml <1055200502@qq.com>
2017-07-13 18:33:49 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,11 +1,9 @@
 // Write your code here
-function Person() {
-     this.name = "Tom";    
-     this.age = 21;
-     this.Introduce = function () {
-         alert("My name is " + this.name + ".I am " + this.age + "years old.");
-     }
-     alert("My name is " + this.name + ".I'm " + this.age + "years old.");
- };
-
+ var Person = function (name, age) {
+            this.name = name;
+            this.age = age;
+            this.Introduce = function () {
+                alert("My name is " + this.name + ".I am " + this.age + "years old.");
+            };
+        };
 module.exports = Person

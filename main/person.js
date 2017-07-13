55032165f3a5df15c7be6597a601e453b03b2d1b
WuYiChenhml <1055200502@qq.com>
2017-07-13 09:50:03 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,7 +1,7 @@
 // Write your code here
 function Person(name，age){
-   this.name = "Tom";//姓名属性
-   this.age = 21;//年龄属性
+   this.name = name;//姓名属性
+   this.age = age;//年龄属性
   
  this. introduce = 'My name is ' + this.name + '. I am ' + this.age + ' years old.';
    return = this.introduce;

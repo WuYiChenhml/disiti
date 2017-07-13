f00f4ee01e855c7757ef5f29e466f3a6cfd93bac
WuYiChenhml <1055200502@qq.com>
2017-07-13 09:50:40 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -5,7 +5,7 @@ function Student(name，age，Class) {
       this.method = Person; 
       this.method(name，age);
       delete this.method; 
-      this.class = 2;
+      this.class = Class;
      
      this.introduce = 'My name is ' + this.name + '. I am ' +  this.age + 'years old. I am a Student. I am at Class ' + this.class + ' .';
     return = this.introduce;

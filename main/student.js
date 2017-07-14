bef3828b9d18b5664ade9aacd588b18e4cf40692
WuYiChenhml <1055200502@qq.com>
2017-07-14 14:10:02 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,7 +1,7 @@
 
 // Write your code here
 function Student(name, age, Class) {
-     this.introduce=function(){
+     this.introduce = function(){
           var str = 'My name is ' + name + '. I am ' + age + ' years old. I am a Student. I am at Class ' + Class + '.';
          return str;
      };

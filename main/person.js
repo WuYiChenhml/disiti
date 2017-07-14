e9797ff18b7e0da8ff4d7531dbf693da02e4de04
WuYiChenhml <1055200502@qq.com>
2017-07-14 14:10:35 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,7 +1,7 @@
 // Write your code here
 function (name, age) {
   this.introduce = function(){
-     var str = "My name is ' + name + '. I am ' + age + ' years old.";
+     var str = "My name is " + name + ". I am " + age + " years old.";
         return str;
     };
         }

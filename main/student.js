8ba1857595fb4dcf6eb374db2e679eefe2b7a12c
WuYiChenhml <1055200502@qq.com>
2017-07-14 14:00:27 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,12 +1,9 @@
 
 // Write your code here
-var Person = function (name, age, class) {
-            this.name = name;
-            this.age = age;
-            this.class = class;
-     this.Introduce = function () {
-         alert('My name is ' + this.name + '. I am ' + this.age + ' years old. I am a Student. I am at Class ' + this.class + '.');
+function Student(name, age, Class) {
+     this.introduce=function(){
+         return 'My name is ' + name + '. I am ' + age + ' years old. I am a Student. I am at Class ' + Class + '.';
      };
- };
+ }
 
 module.exports = Student
